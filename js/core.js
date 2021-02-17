f8e7cfe42de2595eea93949ee949ef8825b949e5
Billy <billy.jakmall@gmail.com>
2021-02-17 12:05:17 +0700

    fix: implement slick slider for home banner

--- js/core.js
+++ js/core.js
@@ -1,2 +1,32 @@
 // @codekit-prepend "vendor/jquery-3.4.1.min.js";
 // @codekit-prepend "vendor/slick.min.js";
+
+
+$(function(){
+  const homeBannerSlides = $('.hb__slides');
+  const homeBannerNextArrow = $('.hb__arrow--next');
+  const homeBannerPrevArrow = $('.hb__arrow--prev');
+
+  homeBannerSlides.slick(
+    slickProps(homeBannerPrevArrow ,homeBannerNextArrow)
+  );
+
+  homeBannerSlides.on('wheel', (function(e) {
+    e.preventDefault();
+    if (e.originalEvent.deltaY < 0) {
+      $(this).slick('slickNext');
+    } else {
+      $(this).slick('slickPrev');
+    }
+  }));
+})
+
+function slickProps(prevSelector, nextSelector){
+  return {
+    slidesToShow: 1,
+    dots: true,
+    speed: 600,
+    prevArrow: prevSelector,
+		nextArrow: nextSelector
+  }
+}
