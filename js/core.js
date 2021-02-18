c3f6dbb09ddbbceeb171a6c0ff7f2fb041f7b32f
Billy <billy.jakmall@gmail.com>
2021-02-18 11:32:43 +0700

    fix: add showcase section

--- js/core.js
+++ js/core.js
@@ -3,15 +3,30 @@
 
 
 $(function(){
+  const allBannerSlides = $('.hb__slides, .sc__banner__slides');
   const homeBannerSlides = $('.hb__slides');
   const homeBannerNextArrow = $('.hb__arrow--next');
   const homeBannerPrevArrow = $('.hb__arrow--prev');
 
+  const showcaseBannerSlides = $('.sc__banner__slides');
+  const showcaseBannerNextArrow = $('.sc__banner__arrow--next');
+  const showcaseBannerPrevArrow = $('.sc__banner__arrow--prev');
+
+  const showcaseDetailSlides = $('.sc__detail__slides');
+
   homeBannerSlides.slick(
-    slickProps(homeBannerPrevArrow ,homeBannerNextArrow)
+    slickProps(1, homeBannerPrevArrow ,homeBannerNextArrow, false, true)
   );
 
-  homeBannerSlides.on('wheel', (function(e) {
+  showcaseBannerSlides.slick(
+    slickProps(3, showcaseBannerNextArrow ,showcaseBannerPrevArrow, true, false, '.sc__detail__slides')
+  );
+
+  showcaseDetailSlides.slick(
+    slickProps(1, '' , '', false, false)
+  );
+
+  allBannerSlides.on('wheel', (function(e) {
     e.preventDefault();
     if (e.originalEvent.deltaY < 0) {
       $(this).slick('slickNext');
@@ -21,10 +36,35 @@ $(function(){
   }));
 })
 
-function slickProps(prevSelector, nextSelector){
+function slickProps(slideShow = 1, prevSelector, nextSelector, isCenter, isUseDots, isUseNav = null){
+  let additionalProps = {}
+  if(isCenter){
+    additionalProps = {
+      initialSlide: 2,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            centerMode: true,
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            centerMode: true,
+            slidesToShow: 1
+          }
+        }
+      ]
+    }
+  }
   return {
-    slidesToShow: 1,
-    dots: true,
+    ...additionalProps,
+    centerMode: isCenter,
+    slidesToShow: slideShow,
+    dots: isUseDots,
+    asNavFor: isUseNav,
     speed: 600,
     prevArrow: prevSelector,
 		nextArrow: nextSelector
